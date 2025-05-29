48d66b916a8dd23773083b4daebcc6731cfa2974
artemmihelson <artem.mihelson@gmail.com>
2025-05-29 10:58:55 +0300

    chore: clone base project and clean up template files

--- TransactionsTestTask/Services/AnalyticsService/AnalyticsService.swift
+++ TransactionsTestTask/Services/AnalyticsService/AnalyticsService.swift
@@ -6,11 +6,6 @@
 
 import Foundation
 
-/// Analytics Service is used for events logging
-/// The list of reasonable events is up to you
-/// It should be possible not only to track events but to get it from the service
-/// The minimal needed filters are: event name and date range
-/// The service should be covered by unit tests
 protocol AnalyticsService: AnyObject {
     
     func trackEvent(name: String, parameters: [String: String])
--- TransactionsTestTask/Services/BitcoinRateService.swift
+++ TransactionsTestTask/Services/BitcoinRateService.swift
@@ -5,10 +5,7 @@
 //
 
 /// Rate Service should fetch data from https://api.coindesk.com/v1/bpi/currentprice.json
-/// Fetching should be scheduled with dynamic update interval
-/// Rate should be cached for the offline mode
-/// Every successful fetch should be logged with analytics service
-/// The service should be covered by unit tests
+
 protocol BitcoinRateService: AnyObject {
     
     var onRateUpdate: ((Double) -> Void)? { get set }
